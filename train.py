dea888d0dd9f9338c0bd4b731e505183a5b06961
sebascorreia <sc01879@surrey.ac.uk>
2023-10-19 18:36:45 +0100

    Added vae.py and training vae function

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 from ae import Autoencoder
+from vae import VAE
 from tensorflow import keras
 from keras.datasets import mnist
 def load_mnist():
@@ -10,8 +11,20 @@ def load_mnist():
     x_test = x_test.reshape(x_test.shape + (1,))
 
     return x_train, y_train, x_test, y_test
-def train(x_train, learning_rate, batch_size, epochs):
+def train_ae(x_train, learning_rate, batch_size, epochs):
     autoencoder = Autoencoder(
+        input_shape=(28, 28, 1),
+        conv_filters=(32, 64, 64, 64),
+        conv_kernels=(3, 3, 3, 3),
+        conv_strides=(1, 2, 2, 1),
+        latent_space_dim=2
+    )
+    autoencoder.summary()
+    autoencoder.compile(learning_rate)
+    autoencoder.train(x_train, batch_size, epochs)
+    return autoencoder
+def train_vae(x_train, learning_rate, batch_size, epochs):
+    autoencoder = VAE(
         input_shape=(28, 28, 1),
         conv_filters=(32, 64, 64, 64),
         conv_kernels=(3, 3, 3, 3),
