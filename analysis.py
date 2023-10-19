ce5722f6aa68b1b77848cd09080020b09f41c7dd
sebascorreia <sc01879@surrey.ac.uk>
2023-10-19 15:15:57 +0100

    added analysis.py

---
+++ analysis.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from ae import Autoencoder
+from train import load_mnist
+
+
+def select_images(images, labels, num_images=10):
+    sample_images_index = np.random.choice(range(len(images)), num_images)
+    sample_images = images[sample_images_index]
+    sample_labels = labels[sample_images_index]
+    return sample_images, sample_labels
+
+
+def plot_reconstructed_images(images, reconstructed_images):
+    fig = plt.figure(figsize=(15, 3))
+    num_images = len(images)
+    for i, (image, reconstructed_image) in enumerate(zip(images, reconstructed_images)):
+        image = image.squeeze()
+        ax = fig.add_subplot(2, num_images, i + 1)
+        ax.axis("off")
+        ax.imshow(image, cmap="gray_r")
+        reconstructed_image = reconstructed_image.squeeze()
+        ax = fig.add_subplot(2, num_images, i + num_images + 1)
+        ax.axis("off")
+        ax.imshow(reconstructed_image, cmap="gray_r")
+    plt.show()
+
+
+def plot_images_encoded_in_latent_space(latent_representations, sample_labels):
+    plt.figure(figsize=(10, 10))
+    plt.scatter(latent_representations[:, 0],
+                latent_representations[:, 1],
+                cmap="rainbow",
+                c=sample_labels,
+                alpha=0.5,
+                s=2)
+    plt.colorbar()
+    plt.show()
+
+
