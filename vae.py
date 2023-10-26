b9d18672ed26e2626778a73949757e661539f811
sebascorreia <sc01879@surrey.ac.uk>
2023-10-26 16:14:22 +0100

    minor fixes to vae

--- vae.py
+++ vae.py
@@ -60,7 +60,7 @@ class VAE:
         self.model.summary()
 
     def compile(self, learning_rate=0.0001):
-        optimizer = Adam(learning_rate=learning_rate)
+        optimizer = keras.optimizers.legacy.Adam(learning_rate=learning_rate)
         self.model.compile(optimizer=optimizer,
                            loss=self._calculate_combined_loss,
                            metrics=[_calculate_reconstruction_loss,
